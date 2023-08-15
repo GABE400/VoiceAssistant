51a4d5747a9e93c90bd76980e26e3c4b2c881a0d
Gabriel <gabechipaya@gmail.com>
2023-08-15 10:58:36 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import {SafeAreaView, Text} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-red-700">
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+}
